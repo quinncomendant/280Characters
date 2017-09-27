9c9b6e4944ee7671e15da06bbadf5927344473ca
Quinn Comendant <quinn@strangecode.com>
2017-09-27 22:26:39 +0530

    Target multiple tweet boxes. Repress 'Uncaught ReferenceError' messages in console. Bump version 1.0.2.

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 Twitter has [released 280-character tweeting](https://blog.twitter.com/official/en_us/topics/product/2017/Giving-you-more-characters-to-express-yourself.html), but only to a small group of people during a trial period. This Chrome extension adds the ability for everyone to tweet in 280 characters, on both [Twitter.com](https://twitter.com/) and [TweetDeck](https://tweetdeck.twitter.com/).
 
-Install the extension on the Chrome Web Store: *(Not available yet: it is awaiting approval)*
-
-There is no tracking code (ehem, unlike the *Tweet 280 Today!* extension). The source code is available here on GitHub and pull requests are invited.
+There is no tracking code (ehem, *Tweet 280 Today*). The source code is available here on GitHub and pull requests are invited.
 
 This extension works thanks to tricks by [@Zemnmez](https://twitter.com/Zemnmez/status/912876877391335424) and [@Prof9](https://twitter.com/Prof9/status/912859110776950784). It's likely Twitter will modify their systems to prevent these hacks, so don't be surprised if it stops working.
--- tweetdeck.js
+++ tweetdeck.js
@@ -1,8 +1,10 @@
 var checkExists = setInterval(function() {
-    if (typeof TD.services.TwitterClient.prototype.makeTwitterCall !== 'undefined' && typeof twttr.txt !== 'undefined') {
-        // Next two lines by @Zemnmez https://twitter.com/Zemnmez/status/912876877391335424
-        TD.services.TwitterClient.prototype.makeTwitterCall = function(b, e, f, g, c, d, h) { c = c || function() {}; d = d || function() {}; e = e || function() {}; b = this.request(b, { method: f, params: Object.assign(e, { weighted_character_count: !0 }), processor: g, feedType: h }); return b.addCallbacks(function(a) { c(a.data) }, function(a) { d(a.req, "", a.msg, a.req.errors) }), b };
-        twttrTxt = Object.assign({}, twttr.txt, { isInvalidTweet: function() { return !1 }, getTweetLength: function() { return twttr.txt.getTweetLength.apply(this, arguments) - 140 } });
-        clearInterval(checkExists);
-    }
+    try {
+        if (typeof TD.services.TwitterClient.prototype.makeTwitterCall !== 'undefined' && typeof twttr.txt !== 'undefined') {
+            // Next two lines by @Zemnmez https://twitter.com/Zemnmez/status/912876877391335424
+            TD.services.TwitterClient.prototype.makeTwitterCall = function(b, e, f, g, c, d, h) { c = c || function() {}; d = d || function() {}; e = e || function() {}; b = this.request(b, { method: f, params: Object.assign(e, { weighted_character_count: !0 }), processor: g, feedType: h }); return b.addCallbacks(function(a) { c(a.data) }, function(a) { d(a.req, "", a.msg, a.req.errors) }), b };
+            twttrTxt = Object.assign({}, twttr.txt, { isInvalidTweet: function() { return !1 }, getTweetLength: function() { return twttr.txt.getTweetLength.apply(this, arguments) - 140 } });
+            clearInterval(checkExists);
+        }
+    } catch (e) {}
 }, 500);

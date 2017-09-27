95b94e225d6a7ce8ffab65433ab2a16d16e15fd4
Quinn Comendant <quinn@strangecode.com>
2017-09-27 19:21:22 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Twitter and TweetDeck in #280Characters
+
+Twitter has [released 280-character tweeting](https://blog.twitter.com/official/en_us/topics/product/2017/Giving-you-more-characters-to-express-yourself.html), but only to a small group of people during a trial period. This Chrome extension adds the ability for everyone to tweet in 280 characters, on both [Twitter.com](https://twitter.com/) and [TweetDeck](https://tweetdeck.twitter.com/).
+
+Install the extension on the Chrome Web Store.
---
+++ twitter.js
@@ -0,0 +1,17 @@
+var checkExists = setInterval(function() {
+    if (document.getElementById('init-data').value != '') {
+        // Next five lines by @Prof9 https://twitter.com/Prof9/status/912859110776950784
+        var initDataInput = document.getElementById('init-data');
+        var initData = JSON.parse(initDataInput.value);
+        initData.deciders.cramming_feature_enabled = true;
+        initData.deciders.cramming_ui_enabled = true;
+        initDataInput.value = JSON.stringify(initData);
+        document.querySelector('.RichEditor-container').insertAdjacentHTML('beforeend', '<div class="RichEditor-rightItems RichEditor-bottomItems"> <div class="js-character-counter"> <div class="js-countdown-counter tweet-counter warn CountdownCounter"></div> <svg class="RadialCounter js-radial-counter" height="20" width="20"> <style> /* Global svg style overrides the overflow. Added svg for specificity */ svg.RadialCounter { margin-bottom: -4px; overflow: visible; transform: rotate(-90deg); } .RadialCounter--safe { stroke: #1da1f2; } .RadialCounter--warn { stroke: #ffad1f; } .RadialCounter--danger { stroke: #e0245e; } .RadialCounter-progressUnderlay { stroke: #ccd6dd; } @keyframes RadialCounterPulse { 0% { stroke-width:2 } 50% { stroke-width: 4; } 100% { stroke-width: 2; } } .RadialCounter--danger.RadialCounter--pulse, .RadialCounter--warn.RadialCounter--pulse { animation: RadialCounterPulse 0.3s ease-in-out; animation-iteration-count: 1; } </style> <circle class="RadialCounter-progressUnderlay" cx="50%" cy="50%" r="8" fill="none" stroke-width="1"></circle> <circle class="js-progress-circle RadialCounter--safe" cx="50%" cy="50%" r="8" fill="none" stroke-width="2" style="stroke-dashoffset: 46.2442; stroke-dasharray: 50.2655;"> </circle> </svg> </div> </div>');
+        clearInterval(checkExists);
+    }
+}, 50);
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.RichEditor-bottomItems > .EmojiPicker').parentElement.classList.add('RichEditor-topItems');
+    document.querySelector('.RichEditor-bottomItems > .EmojiPicker').parentElement.classList.remove('RichEditor-bottomItems');
+    document.querySelector('.TweetBoxToolbar-tweetButton > span.tweet-counter').style.display = 'none';
+}, false);

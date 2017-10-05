ad11cad60fb15cb8fa05e2c6d91fc6d46e2b01cf
Quinn Comendant <quinn@strangecode.com>
2017-10-05 22:35:33 +0530

    TweetDeck blocked the hack. Updated the extension so it doesn't execute anything for users who still have it installed. Update extension description to note that this extension does nothing.

--- content.js
+++ content.js
@@ -1,13 +1,13 @@
-var s = document.createElement('script');
-switch (location.hostname) {
-case 'tweetdeck.twitter.com':
-    s.src = chrome.extension.getURL('tweetdeck.js');
-    break;
-case 'twitter.com':
-    s.src = chrome.extension.getURL('twitter.js');
-    break;
-}
-s.onload = function() {
-    s.parentNode.removeChild(s);
-};
-(document.head || document.documentElement).appendChild(s);
+// var s = document.createElement('script');
+// switch (location.hostname) {
+// case 'tweetdeck.twitter.com':
+//     s.src = chrome.extension.getURL('tweetdeck.js');
+//     break;
+// case 'twitter.com':
+//     s.src = chrome.extension.getURL('twitter.js');
+//     break;
+// }
+// s.onload = function() {
+//     s.parentNode.removeChild(s);
+// };
+// (document.head || document.documentElement).appendChild(s);
